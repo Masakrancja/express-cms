fcc3d09c41b5114805b4afef9071a052583562c7
Piotr Pietrzak <piotrpietrzak145@gmail.com>
2024-07-19 13:53:54 +0200

    quiz firm

--- config.js
+++ config.js
@@ -1,5 +1,14 @@
 module.exports = {
   keysSessions: ["TAJNY_KLUCZ"],
   maxAgeSession: 1 * 60 * 60 * 1000,
-  uri: "",
+  uri: "mongodb+srv://piotrpietrzak145:3zCBcQXFGFypQPsn@clustermongodb.9nqlztx.mongodb.net/cms?retryWrites=true&w=majority&appName=ClusterMongoDB",
 };
+
+//MongoDB
+//login: piotrpietrzak145
+//Passwd: 3zCBcQXFGFypQPsn
+
+/*
+mongosh "mongodb+srv://clustermongodb.9nqlztx.mongodb.net/" --apiVersion 1 --username piotrpietrzak145 --password 3zCBcQXFGFypQPsn
+
+*/
--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,49 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const Quiz = require("../models/quiz");
+const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("quiz", { title: "Quiz" });
+  const show = !req.session.vote;
+  const o = {};
+  o["title"] = "Quiz";
+  o["data"] = [];
+  o["message"] = "";
+  o["show"] = show;
+  (async () => {
+    return Quiz.find();
+  })()
+    .then((data) => {
+      const sum = data.reduce((a, b) => a + b.vote, 0);
+      o["data"] = data;
+      o["sum"] = sum;
+      res.render("quiz", o);
+    })
+    .catch((err) => {
+      o["message"] = err.message;
+      res.render("quiz", o);
+    });
+});
+
+router.post("/", (req, res) => {
+  const o = {};
+  o["title"] = "Quiz";
+  o["data"] = [];
+  o["message"] = "";
+  (async () => {
+    const id = req.body.quiz;
+    const data = await Quiz.findOne({ _id: id });
+    data.vote += 1;
+    req.session.vote = 1;
+    return data.save();
+  })()
+    .then((data) => {
+      res.redirect("/quiz");
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.render("quiz", o);
+    });
 });
 
 module.exports = router;
