cdc3761fd60c57874c04c467dc87b3ab39dccfac
Piotr Pietrzak <piotrpietrzak145@gmail.com>
2024-07-19 10:27:37 +0200

    news filter

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,33 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const News = require("../models/news");
+const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("news", { title: "News" });
+  const search = req.query.search || "";
+  const o = {};
+  o["title"] = "News";
+  o["message"] = "";
+  o["data"] = [];
+  o["search"] = search;
+
+  (async () => {
+    return await News.find(
+      search ? { title: new RegExp(search, "i") } : {}
+    ).sort({
+      created: -1,
+    });
+  })()
+    .then((data) => {
+      o["data"] = data;
+      console.log(o);
+      res.render("news", o);
+    })
+    .catch((err) => {
+      o["message"] = err.message;
+      console.log(o);
+      res.render("news", o);
+    });
 });
 
 module.exports = router;
